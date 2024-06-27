f973898f8bd3f5ef290d373a08afd5e1b8fda19b
kth232 <kth7537@naver.com>
2024-06-27 17:24:25 +0900

    게시판 추가

--- src/main/java/org/choongang/board/entities/Board.java
+++ src/main/java/org/choongang/board/entities/Board.java
@@ -10,7 +10,7 @@ public class Board {
     private String bId; //게시파 아이디
     private String bName; //게시판 이름
     private int rowsPerPage; //1페이지 행 수
-    private int actice; //사용 여부
+    private int active; //사용 여부
     private int activeCategory; //분류 사용 여부
     private int category; //분류
     private Authority authority;
